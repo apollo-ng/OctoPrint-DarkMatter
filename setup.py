9358e7ca6192a99f66fce31c9503c67614885cfb
chrono <chrono@open-resource.org>
2017-10-29 18:45:35 +0000

    Bump to 0.1.16

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 plugin_identifier = "darkmatter"
 plugin_package = "octoprint_darkmatter"
 plugin_name = "OctoPrint-DarkMatter"
-plugin_version = "0.1.15"
+plugin_version = "0.1.16"
 
 plugin_description = "DarkMatter theme adapted for OctoPrint"
 plugin_author = "chrono"
